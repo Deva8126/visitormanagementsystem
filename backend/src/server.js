04893c30e39088e7685bb74db427b142311d816c
Deva8126 <devvanshgupta@gmail.com>
2026-08-12 18:18:23 +0530

    dist in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../public/dist")));
 
 // Configure body parsing with increased size limit to hold webcam image payloads
 app.use(express.json({ limit: '10mb' }));
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../public/dist/index.html"));
 });
 
 // Start CGST Backend API Server
